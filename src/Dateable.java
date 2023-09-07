a863f473866e6bdfea22c1e34cf514f37f7ef848
Blearybear <timh2026@student.cis.edu.hk>
2023-09-07 19:08:08 +0800

    Finished CS 11.01 Lab 1.1 [version 1]

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = -1;
+        while (age<0) {
+            try {
+                age = Integer.parseInt(myScanner.nextLine());
+            }
+            catch (NumberFormatException e){
+                System.out.println("Please enter a valid number.");
+            }
+        }
+        int datingAge = (age/2) + 7;
+        System.out.println("You are " + age + " years old, therefore you should be dating someone who is at least " + datingAge + ".");
     }
+
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = -1;
+        while (age<0) {
+            try {
+                age = Integer.parseInt(myScanner.nextLine());
+            }
+            catch (NumberFormatException e){
+                System.out.println("Please enter a valid number.");
+            }
+        }
+        int readPages = 100 - age;
+        System.out.println("You are " + age + " years old, therefore you should read the first " + readPages + " pages of a book before giving up.");
     }
 }
